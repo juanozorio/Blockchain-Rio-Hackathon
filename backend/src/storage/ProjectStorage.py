8ee14c390cf978c031048bcd60b4ca22be45a5c6
Guilherme Santos <guilhermetecsouza@gmail.com>
2024-07-16 12:32:46 -0300

    correction in post method

--- backend/src/storage/ProjectStorage.py
+++ backend/src/storage/ProjectStorage.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime
 import logging
 from typing import List, Optional
-
+from model.ProjectModel import ProjectModel
 # Registrando adaptadores e conversores de datetime
 def adapt_datetime(ts):
     return ts.strftime('%Y-%m-%d %H:%M:%S')
@@ -16,28 +16,6 @@ def convert_datetime(s):
 sqlite3.register_adapter(datetime.datetime, adapt_datetime)
 sqlite3.register_converter("timestamp", convert_datetime)
 
-# Classe de modelo
-class ProjectModel:
-    def __init__(self, id: Optional[int], name: str, icon: str, banner: str, wallet: str, bio: str, project_type: bool, description: str, created_at: datetime.datetime = None, updated_at: datetime.datetime = None):
-        self.id = id
-        self.name = name
-        self.icon = icon
-        self.banner = banner
-        self.wallet = wallet
-        self.bio = bio
-        self.project_type = project_type
-        self.description = description
-        self.created_at = created_at or datetime.datetime.now()
-        self.updated_at = updated_at or datetime.datetime.now()
-
-    def get_wallet(self) -> str:
-        return self.wallet
-
-    def __repr__(self):
-        return f"Project(id={self.id}, name='{self.name}', icon='{self.icon}', banner='{self.banner}', wallet='{self.wallet}', bio='{self.bio}', project_type={self.project_type}, descript='{self.descript}', created_at={self.created_at}, updated_at={self.updated_at})"
-
-
-# Classe de camada de armazenamento
 class ProjectStorage:
     def __init__(self, db_name: str = "project.db"):
         self.db_name = db_name
@@ -66,7 +44,7 @@ class ProjectStorage:
         with sqlite3.connect(self.db_name, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
             cursor = conn.cursor()
             cursor.execute("""
-            INSERT INTO projects (name, icon, banner, wallet, bio, project_type, descript, created_at, updated_at)
+            INSERT INTO projects (name, icon, banner, wallet, bio, project_type, description, created_at, updated_at)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             """, (project.name, project.icon, project.banner, project.wallet, project.bio, project.project_type, project.description, project.created_at, project.updated_at))
             conn.commit()
@@ -93,7 +71,7 @@ class ProjectStorage:
             cursor = conn.cursor()
             cursor.execute("""
             UPDATE projects
-            SET name = ?, icon = ?, banner = ?, wallet = ?, bio = ?, project_type = ?, descript = ?, created_at = ?, updated_at = ?
+            SET name = ?, icon = ?, banner = ?, wallet = ?, bio = ?, project_type = ?, description = ?, created_at = ?, updated_at = ?
             WHERE id = ?
             """, (project.name, project.icon, project.banner, project.wallet, project.bio, project.project_type, project.description, project.created_at, project.updated_at, project.id))
             conn.commit()
