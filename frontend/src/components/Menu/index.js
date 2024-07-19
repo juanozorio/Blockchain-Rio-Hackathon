ecc73b186c272e5e583f82f8115ea42853516e3e
Raph2ll <raphael.ph2ll@gmail.com>
2024-07-19 00:03:19 -0300

    fix: nav css

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -1,23 +1,25 @@
 import Link from 'next/link';
-import styles from './Menu.module.css';
 import { FaSearch } from 'react-icons/fa';
 import Taba from '../Taba/Taba';
 
 const Menu = () => {
   return (
-    <nav className="p-6 flex justify-around max-w-full max-h-26 pt-16 ">
-      <ul className="align-center flex items-center text-xl text-cyan-50 gap-20 ">
-      <Taba/>
-        <li className="border-b-2 px-2 pt-2 pb-2 border-transparent hover:border-cyan-50">
+    <nav className="px-20 flex justify-center max-w-full max-h-26 pt-16">
+      <Taba />
+      <ul className="flex w-full items-center text-xl text-cyan-50 pl-20">
+        <li className="flex-1 text-center border-b-2 px-4 py-2 border-transparent hover:border-cyan-50">
           Home
         </li>
-        <li className="border-b-2 px-2 pt-2 pb-2 border-transparent hover:border-cyan-50">
+        <li className="flex-1 text-center border-b-2 px-4 py-2 border-transparent hover:border-cyan-50">
           About us
         </li>
-        <li className="border-b-2 px-2 pt-2 pb-2 border-transparent hover:border-cyan-50">
-          <Link href="/IniciarProjeto">Projects </Link>
+        <li className="flex-1 text-center border-b-2 px-4 py-2 border-transparent hover:border-cyan-50">
+          <Link href="/IniciarProjeto">Projects</Link>
         </li>
-        <li className="border-b-2 px-2 pt-2 pb-2 border-transparent hover:border-cyan-50">
+        <li className="flex-1 text-center border-b-2 px-4 py-2 border-transparent hover:border-cyan-50">
+          Contact Us
+        </li>
+        <li className="flex-1 text-center border-b-2 px-4 py-2 border-transparent hover:border-cyan-50">
           Contact Us
         </li>
       </ul>
