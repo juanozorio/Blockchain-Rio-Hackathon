4c09c80c89ccbe7b625d0de779fa9267e791c973
Raph2ll <raphael.ph2ll@gmail.com>
2024-07-19 00:07:05 -0300

    fix: pl ul

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -6,7 +6,7 @@ const Menu = () => {
   return (
     <nav className="px-20 flex justify-center max-w-full max-h-26 pt-16">
       <Taba />
-      <ul className="flex w-full items-center text-xl text-cyan-50 pl-20">
+      <ul className="flex w-full items-center text-xl text-cyan-50 pl-16">
         <li className="flex-1 text-center border-b-2 px-4 py-2 border-transparent hover:border-cyan-50">
           Home
         </li>
