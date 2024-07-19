d5c265e0db3992798c1c29d0311e368e5a1fca28
Raph2ll <raphael.ph2ll@gmail.com>
2024-07-19 00:15:12 -0300

    feat: SubMenu

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,14 +2,17 @@
 import CarouselBanner from '../components/CarouselBanner/CarouselBanner';
 import Menu from '../components/Menu';
 import Card from '../components/Card';
+import Taba from '@/components/Taba/Taba';
+import SubMenu from '@/components/SubMenu/Index';
 
 export default function Home() {
   return (
     <>
-      <Menu />
+      <Menu className="flex justify-end max-w-xl" />
+      <SubMenu />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <CarouselBanner />
-        <br />
+      <CarouselBanner />
+
         <Card description={'aloha caption'} title={'title aqui'} />
       </main>
     </>
---
+++ frontend/src/components/SubMenu/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SubMenu = () => {
+  return (
+    <div className='pt-32 px-20 max-w-2xl text-cyan-50'>
+      <p className='text-4xl'>
+        Welcome To
+      </p>
+      <p className='text-6xl pb-4'>
+        Best Design Site
+      </p>
+      <p className='text-xl'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec sollicitudin arcu. Fusce sed imperdiet lectus, nec convallis turpis. 
+      </p>
+    </div>
+  );
+};
+
+export default SubMenu;
